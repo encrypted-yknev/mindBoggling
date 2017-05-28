bd725e5321c6f1907f76146ad6ff3964132cd608
N Venkateshan <venkycse93@gmail.com>
2017-05-28 21:30:12 +0530

    Add files via upload
    Home page

---
+++ home.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php include "header.php" ?>
+<h2>Welcome Folks!</h2>
+</br>
+
+<div>
+	<a href="register.php" target="_blank">New User? Sign up</a></br></br>
+	<a href="login.php" target="_blank">Existing User? Login</a></br></br>
+	
+</div>
+<?php include "footer.php" ?>
+</body>
+</html>
